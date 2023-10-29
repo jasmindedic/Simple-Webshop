920c6403b2ac8c10a040c9bfa0cd6296bd58ef95
jasmindedic <jdedic1999@gmail.com>
2023-10-29 17:07:03 +0100

    Added real redirections to my footer links

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ function getById(elementName) {
 function init() {
     generateNavBar();
     renderProducts();
+    generateFooter();
 };
 
 
--- templates/js_templates/footer.js
+++ templates/js_templates/footer.js
@@ -1,10 +1,18 @@
 // Get footer
 let footerContainer = getById("footer");
 
-
+// Function to generate header
+function generateFooter() {
+    footerContainer.innerHTML = createFooter();
+}
 
 function createFooter(){
     return /* html */`
-        
+    <div class="links">
+        <a href="https://www.instagram.com/" target="_blank"><i class="fa-brands fa-square-instagram"></i></a>
+        <a href="https://twitter.com/?lang=de" target="_blank"><i class="fa-brands fa-square-x-twitter"></i></a>
+        <a href="https://about.meta.com/de/" target="_blank"><i class="fa-brands fa-meta"></i></a>
+        <a href="https://github.com/" target="_blank"><i class="fa-brands fa-square-github"></i></a>
+    </div>
     `;
 };
